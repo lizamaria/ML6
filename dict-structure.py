8d8096af62334e8435cece0f8cdd45a40f2bd1fd
Maja Minnaert <maja.minnaert@gmail.com>
2021-03-01 11:31:12 +0100

    set up dict-structure.py

---
+++ dict-structure.py
@@ -0,0 +1,10 @@
+# This file contains the skeleton of the dictionary
+# You can use this skeleton to store the extracted values
+# for each of the key's, there should be space for each of
+# the key's that can be extracted from the provided pdf files
+
+# This is a work in progress
+# Todo: list top levels
+# Todo: nest subdictionaries
+
+pdf_information = {'Identification': None}
