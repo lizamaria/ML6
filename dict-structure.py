d9ce9600d4cd7e248b1d1a54494e408ec1c8c115
Maja Minnaert <maja.minnaert@gmail.com>
2021-03-02 14:10:46 +0100

    update dict-structure.py; added todo-item and testcode tweaks

--- dict-structure.py
+++ dict-structure.py
@@ -3,10 +3,12 @@
 # for each of the key's, there should be space for each of
 # the key's that can be extracted from the provided pdf files
 
+
 # This is a work in progress
 # Currently tested for list of headers and list of key-value dicts
 # that correspond by order in the respective lists
 # Todo: find way to extend a level of the tree (currently checking conversion dict -> list of dicts approach)
+# Todo: find a way to directly assign existing and new keys (and values) to a nested dict
 
 
 def create_skeleton_dict(headers: list, keyvals: list):
@@ -28,9 +30,9 @@ def create_skeleton_dict(headers: list, keyvals: list):
 if __name__ == "__main__":
     import json
     header_list = ["header1", "header2", "header3"]
-    keyval_list = [{"key1": "val1", "key2": "val2", "key3": "val3"},
-                   {"key1": "val1", "key2": "val2", "key3": "val3"},
-                   {"key1": "val1", "key2": "val2", "key3": "val3"}]
+    keyval_list = [{"key1.1": "val1.1", "key1.2": "val1.2", "key1.3": "val1.3"},
+                   {"key2.1": "val2.1", "key2.2": "val2.2", "key2.3": "val2.3"},
+                   {"key3.1": "val3.1", "key3.2": "val3.2", "key3.3": "val3.3"}]
     print(create_skeleton_dict(header_list, keyval_list))
     print(type(create_skeleton_dict(header_list, keyval_list)))
     print(json.dumps(create_skeleton_dict(header_list, keyval_list), indent=2))
