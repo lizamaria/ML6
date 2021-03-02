d70484f19c7b6b9140edc290d53a41903d806869
Maja Minnaert <maja.minnaert@gmail.com>
2021-03-02 13:53:18 +0100

    update parameter info in dict-structure.py

--- dict-structure.py
+++ dict-structure.py
@@ -13,22 +13,24 @@ def create_skeleton_dict(headers: list, keyvals: list):
     """Makes nested dict based on list and dict or list
     *currently only supports one level down of nesting*
 
-    Parameters: a list of headers and a list of dicts of equal length
+    Parameters: a list of headers and a list of dicts where
+    the lists are of equal length (the length of the dicts
+    can vary without causing issue.
 
     Returns: a nested dictionary where respective 'keyvals' are
     nested in respective 'headers' based on order in the list
     and dictionary provided as parameters
     """
     skeleton_dict = dict(zip(headers, keyvals))
-    skeleton_dict = skeleton_dict.items()
     return skeleton_dict
 
 
 if __name__ == "__main__":
+    import json
     header_list = ["header1", "header2", "header3"]
     keyval_list = [{"key1": "val1", "key2": "val2", "key3": "val3"},
                    {"key1": "val1", "key2": "val2", "key3": "val3"},
                    {"key1": "val1", "key2": "val2", "key3": "val3"}]
     print(create_skeleton_dict(header_list, keyval_list))
-    print(create_skeleton_dict(header_list, keyval_list))
+    print(json.dumps(create_skeleton_dict(header_list, keyval_list)))
     print(type(create_skeleton_dict(header_list, keyval_list)))
