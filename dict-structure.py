1cee8d1b1eab0dd1c25ef6ea290789514107b37a
Maja Minnaert <maja.minnaert@gmail.com>
2021-03-02 17:14:36 +0100

    update dict-structure.py

--- dict-structure.py
+++ dict-structure.py
@@ -8,10 +8,12 @@
 # Currently tested for list of headers and list of key-value dicts
 # that correspond by order in the respective lists
 # Todo: find way to build an extra level on the tree
+# Todo: find way to map dictionary to top level headers
+# Todo: discuss option of only delivering a three-level json
 # Todo: (alternate route) find a way to directly assign existing and new keys (and values) to a nested dict
 
 
-def create_skeleton_dict(headers: list, keyvals: list):
+def create_skeleton_dict(headers, keyvals):
     """Makes nested dict based on list and dict or list
     *currently only supports one level down of nesting*
 
@@ -23,18 +25,25 @@ def create_skeleton_dict(headers: list, keyvals: list):
     nested in respective 'headers' based on order in the list
     and dictionary provided as parameters
     """
-    skeleton_dict = dict(zip(headers, keyvals))
-    return skeleton_dict
+    data = dict(zip(headers, keyvals))
+    with open("data.json", "w") as write_file:
+        json.dump(data, write_file, indent=4)
+    return data
 
 
 if __name__ == "__main__":
     import json
-    header_list = ["header1", "header2", "header3"]
+    header_list = ["header1", "header2", "header3", "header4"]
     keyval_list = [{"key1.1": "val1.1", "key1.2": "val1.2", "key1.3": "val1.3"},
                    {"key2.1": "val2.1", "key2.2": "val2.2", "key2.3": "val2.3"},
-                   {"key3.1": "val3.1", "key3.2": "val3.2", "key3.3": "val3.3"}]
-    print(create_skeleton_dict(header_list, keyval_list))
-    print(type(create_skeleton_dict(header_list, keyval_list)))
-    data = create_skeleton_dict(header_list, keyval_list)
-    with open("data.json", "w") as write_file:
-        json.dump(data, write_file, indent=4)
+                   {"key3.1": "val3.1", "key3.2": "val3.2", "key3.3": "val3.3"},
+                   "value4"]
+    branches = create_skeleton_dict(header_list, keyval_list)
+    print(branches)
+    print(type(branches))
+
+    top_headers = ["top1", "top2", "top3", "top4"]
+
+    tree = create_skeleton_dict(top_headers, branches)
+    print(tree)
+    print(type(tree))
