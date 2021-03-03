c977195295f1a740611f11192c1b6c61f7b51067
Maja Minnaert <maja.minnaert@gmail.com>
2021-03-03 11:07:23 +0100

    update pdf-to-text.py; packeged function, added docstring

--- pdf-to-text.py
+++ pdf-to-text.py
@@ -1,16 +1,26 @@
-import pandas as pd
+# This file contains a function that extracts all text in a pdf
+# Useful for doing extraction operations based on only the tex
+# of a pdf
+
+
+# todo: test this function
+
 import PyPDF2
 
-# todo: #1 make callable function out of this
-# todo: #2 convert hard-codings to soft-codings
-# todo: #3 fix pypdf2 import problem (may be local)
 
-pyfile=open('23114.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pyfile)
+def get_pdf_text(path):
+    """Renders all the text, with font informtion
+    intact, of a pdf file in specified path
 
-# create page object and extract text
-for page in range(11):
-    pageObj = pdfReader.getPage(page)
-    pageall = pageObj.extractText()
+    :parameter: path to pdf file ex "file.pdf"
 
-print(pageall)
+    :returns: pdf_text variable of type string, containing
+    all text in the pdf along with font, paragraph, newline,...
+    information
+    """
+    with open(path, 'rb'):
+        pdfReader = PyPDF2.PdfFileReader(pyfile)
+        for page in range(11):
+            pageObj = pdfReader.getPage(page)
+            pdf_text = pageObj.extractText()
+        return pdf_text
