120bf3b256270c451a9cab7256e71cd3bd3f6137
Maja Minnaert <maja.minnaert@gmail.com>
2021-03-03 14:54:00 +0100

    clean up branch header-extraction; initial set up header-extract.py

---
+++ header-extract.py
@@ -0,0 +1,21 @@
+# This file contains the functions needed to extract the
+# top level section headers in a pdf file and outputs them
+# as a list of headers
+
+# This is a work in progress
+# todo: copy needed functions
+# todo: write test/demo section under this file (if name is main)
+# todo: test functionality
+# todo: top level functionality in new function
+# todo: write docstring
+# todo: determine and code needed-only imports
+
+# packages used in this file
+
+# backbone functions
+
+# wrapper functions
+
+# testing grounds
+if __name__ == __main__:
+    print("What's up doc?")
